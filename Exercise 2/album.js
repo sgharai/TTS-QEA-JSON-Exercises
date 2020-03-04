090366e759e1f0994bd8bb4a772e6fbb6cf6d6ad
sgharai <sofiagharaibeh@gmail.com>
2020-03-04 18:56:27 -0500

    gets rid of extra files

--- Exercise 2/album.js
+++ Exercise 2/album.js
@@ -1,6 +1,5 @@
 let albumId = localStorage.getItem("albumId")
 
-
 fetch(`http://jsonplaceholder.typicode.com/albums/${albumId}/photos`)
 .then(function(response) {
     return response.json();
@@ -12,9 +11,36 @@ fetch(`http://jsonplaceholder.typicode.com/albums/${albumId}/photos`)
     document.getElementById("header").appendChild(albumTitle)
 
     for (photo in data) {
-        let photoPost = document.createElement("h3")
+        let photoPost = document.createElement("li")
         photoPost.setAttribute("id", "photoTitle")
-        photoPost.innerHTML = `<img src=${data[photo].thumbnailUrl}><br> Title: ${data[photo].title}`
+        photoPost.innerHTML = `<img src=${data[photo].thumbnailUrl}>
+        <br> <h3>${data[photo].title}</h3>`
         document.getElementById("album").appendChild(photoPost);
     }
 })
+
+.then(function(myFunction) {
+    document.getElementById("myInput").addEventListener('onkeyup', myFunction )
+})
+
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("album");
+  li = ul.getElementsByTagName('li');
+
+  console.log(li)
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    h3 = li[i].getElementsByTagName("h3")[0];
+    txtValue = h3.textContent || h3.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
